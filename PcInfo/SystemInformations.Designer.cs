57a09e265338e5fa71c544e83551c6680803277e
mintech-dot <75437079+mintech-dot@users.noreply.github.com>
2021-07-13 12:17:19 +0100

    Add files via upload

---
+++ PcInfo/SystemInformations.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PcInfo
+{
+    partial class SystemInformations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SystemInformations));
+            this.harddisk = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.motherboard = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cpu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mac = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ClientName = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // harddisk
+            // 
+            this.harddisk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.harddisk.Location = new System.Drawing.Point(242, 192);
+            this.harddisk.Name = "harddisk";
+            this.harddisk.Size = new System.Drawing.Size(190, 21);
+            this.harddisk.TabIndex = 0;
+            this.harddisk.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hard Disk Serial Number :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Motherboard Serial Number :";
+            // 
+            // motherboard
+            // 
+            this.motherboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.motherboard.Location = new System.Drawing.Point(242, 308);
+            this.motherboard.Name = "motherboard";
+            this.motherboard.Size = new System.Drawing.Size(190, 21);
+            this.motherboard.TabIndex = 2;
+            this.motherboard.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CPU Number :";
+            // 
+            // cpu
+            // 
+            this.cpu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpu.Location = new System.Drawing.Point(242, 250);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(190, 21);
+            this.cpu.TabIndex = 6;
+            this.cpu.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MAC address :";
+            // 
+            // mac
+            // 
+            this.mac.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mac.Location = new System.Drawing.Point(242, 364);
+            this.mac.Name = "mac";
+            this.mac.Size = new System.Drawing.Size(190, 21);
+            this.mac.TabIndex = 8;
+            this.mac.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Computer Name :";
+            // 
+            // CName
+            // 
+            this.CName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CName.Location = new System.Drawing.Point(242, 135);
+            this.CName.Name = "CName";
+            this.CName.Size = new System.Drawing.Size(190, 21);
+            this.CName.TabIndex = 10;
+            this.CName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Client Name :";
+            // 
+            // ClientName
+            // 
+            this.ClientName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientName.Location = new System.Drawing.Point(242, 76);
+            this.ClientName.Name = "ClientName";
+            this.ClientName.Size = new System.Drawing.Size(190, 21);
+            this.ClientName.TabIndex = 12;
+            this.ClientName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.ForeColor = System.Drawing.Color.White;
+            this.save.Location = new System.Drawing.Point(181, 408);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(96, 32);
+            this.save.TabIndex = 14;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-9, 428);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // SystemInformations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 463);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ClientName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.mac);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.motherboard);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.harddisk);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SystemInformations";
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.Text = "System Information";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox harddisk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox motherboard;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cpu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox mac;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ClientName;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PcInfo/SystemInformations.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+namespace PcInfo
+{
+    public partial class SystemInformations : MetroFramework.Forms.MetroForm
+    {
+        public SystemInformations()
+        {
+            InitializeComponent();
+        }
+        // create function to get Hard disk serial number
+        private void Hard_Disk()
+        {
+            try
+            {
+                ManagementObjectSearcher HD = new ManagementObjectSearcher("SELECT SerialNumber From win32_DiskDrive where SerialNumber IS NOT NULL AND MediaType ='Fixed hard disk media'");
+                foreach(ManagementObject hd in HD.Get())
+                {
+                    //Display the serial number in the textbox
+                    this.harddisk.Text = hd["SerialNumber"].ToString();
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        // create function to get CPU serial number
+        private void CPU()
+        {
+            try
+            {
+                ManagementObjectSearcher CPU = new ManagementObjectSearcher("SELECT ProcessorID From win32_Processor where ProcessorID IS NOT NULL ");
+                foreach (ManagementObject cpu in CPU.Get())
+                {
+                    //Display the serial number in the textbox
+                    this.cpu.Text = cpu["ProcessorID"].ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        // create function to get MotherBoard serial number
+        private void MB()
+        {
+            try
+            {
+                ManagementObjectSearcher MB = new ManagementObjectSearcher("SELECT SerialNumber From win32_BaseBoard where SerialNumber IS NOT NULL ");
+                foreach (ManagementObject mb in MB.Get())
+                {
+                    //Display the serial number in the textbox
+                    this.motherboard.Text = mb["SerialNumber"].ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        // create function to get MacAddress serial number
+        private void MAC()
+        {
+            try
+            {
+                ManagementObjectSearcher MAC = new ManagementObjectSearcher("SELECT MacAddress FROM win32_NetworkAdapter where MacAddress IS NOT NULL AND pnpdeviceid Like 'PCI%'");
+                foreach (ManagementObject mac in MAC.Get())
+                {
+                    //Display the serial number in the textbox
+                    this.mac.Text = mac["MacAddress"].ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        // create function to get Computer name
+        private void name()
+        {
+            string strComputerName = Environment.MachineName.ToString();
+            //Display computer name in textbox
+            this.CName.Text = strComputerName;
+        }
+
+
+    //call all functoins 
+    private void Form1_Load(object sender, EventArgs e)
+        {
+            this.name();
+            this.Hard_Disk();
+            this.CPU();
+            this.MB();
+            this.MAC();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string save = ClientName.Text;
+            try
+            {
+                StreamWriter writer = new StreamWriter( save + ".txt");
+                writer.WriteLine("Client Name :" + this.ClientName.Text);
+                writer.WriteLine("Computer Name : :" + this.CName.Text);
+                writer.WriteLine("Hard Disk Serial Number : :" + this.harddisk.Text);
+                writer.WriteLine("CPU Number ::" + this.cpu.Text);
+                writer.WriteLine("Motherboard Serial Number : :" + this.motherboard.Text);
+                writer.WriteLine("MAC address : :" + this.mac.Text);
+                writer.Close();
+                MessageBox.Show("The information has been saved successfully !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            About ab = new About();
+            ab.ShowDialog();
+        }
+    }
+}
